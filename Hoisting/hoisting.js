8e2712cb2df41039983893e6ba8473eaa736fec6
akyarbrough <ayarbrough0816@gmail.com>
2022-08-31 22:00:43 -0500

    Hoisting
    Beginning of hoisting video.

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,7 @@
+//**Hoisting is when JavaScript declares variables/functions at the top of their scope regardless of where they are at.
+
+//x = 1;
+console.log(z);//undefined
+let x = 0;
+const y = 1;
+var z = 2;
