322e4db9dc563d3645f94dd14df1d22630e37003
akyarbrough <ayarbrough0816@gmail.com>
2022-09-01 19:25:24 -0500

    Commit

---
+++ Day 6/Exercise 1/main.js
@@ -0,0 +1,11 @@
+let name = "John";
+
+function sayHi(){
+    alert("Hi, " + name);
+}
+name = "Pete";
+
+sayHi();
+
+//Prediction: will show Pete
+//Initially showed not defined, but created a new html file and it displayed correctly
--- Warm Up 6/warm_up.js
+++
@@ -1,5 +0,0 @@
-function countTo(num){
-    for (let i = 0; i <= num; i++)
-    console.log(i);
-}
-countTo(5);
